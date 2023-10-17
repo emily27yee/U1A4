f2dd7de65b3a39f86976871254405e121c3f4dac
335151478 <335151478@CL-2UA44112JL.tdsb.on.ca>
2023-10-17 11:36:05 -0400

    commit

---
+++ src/main/java/com/mycompany/u1a4_emilyyee/U1A4_EmilyYee.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.u1a4_emilyyee;
+
+/**
+ *
+ * @author 335151478
+ */
+public class U1A4_EmilyYee {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
